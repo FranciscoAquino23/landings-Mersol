5f3195740896df2c1f5f474412d6c5403cfa331e
Francisco Aquino <francoaquialla@gmail.com>
2026-03-20 12:57:04 -0600

    App route and demo components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/landing-demo.component/landing-demo.component').then(
+        (m) => m.LandingDemoComponent,
+      ),
+    data: {
+      title: 'Mersol - Distribuidores de equipos de soldadura',
+      description: '¡La solución a tus necesidades industriales!.',
+      ogImage: 'assets/landing/landing-demo/og-main.jpg',
+    },
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/pages/landing-demo.component/landing-demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-demo.component.html',
+  styleUrl: './landing-demo.component.scss',
+})
+export class LandingDemoComponent {}
