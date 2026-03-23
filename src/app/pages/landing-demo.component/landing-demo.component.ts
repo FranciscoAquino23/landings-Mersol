d970d0a0ef93cdc3c2575a824bb859c1b39cf9a5
Francisco Aquino <francoaquialla@gmail.com>
2026-03-23 13:32:12 -0600

    Global Architecture readjusted

--- src/app/pages/landing-demo.component/landing-demo.component.ts
+++ src/app/pages/landing-demo.component/landing-demo.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+// Componente principal de la landing page
 @Component({
   selector: 'app-landing-demo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-demo.component.html',
   styleUrl: './landing-demo.component.scss',
 })
-export class LandingDemoComponent {}
+export class LandingDemoComponent {
+  // Gestionar lógica de la aplicación
+}
