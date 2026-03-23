68a47dfab9f0ec7831569735975017c99240cb15
Francisco Aquino <francoaquialla@gmail.com>
2026-03-23 13:16:27 -0600

    Business Identity generated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,25 @@
 import { Routes } from '@angular/router';
 
+// Configurar rutas principales
 export const routes: Routes = [
   {
     path: '',
+    // Verificar coincidencia de la ruta con estructura de código
     loadComponent: () =>
       import('./pages/landing-demo.component/landing-demo.component').then(
         (m) => m.LandingDemoComponent,
       ),
+
+    // Metadatos (Open Graph)
     data: {
-      title: 'Mersol - Distribuidores de equipos de soldadura',
-      description: '¡La solución a tus necesidades industriales!.',
+      title: 'Mersol | Soluciones Industriales y Distribuidor Austromex',
+      description:
+        'Expertos en equipos de soldadura y consumibles industriales. La solución integral para tu empresa.',
       ogImage: 'assets/landing/landing-demo/og-main.jpg',
     },
   },
   {
+    // Manejar URL's no definidas (Error - 404)
     path: '**',
     redirectTo: '',
     pathMatch: 'full',
