e6d7023e4deed6d364ab31d39cd2058377ef630a
Francisco Aquino <francoaquialla@gmail.com>
2026-03-30 08:51:35 -0600

    Nabvar component design (Desktop / Mobile) completed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 /* ==========================================================================
-     NAVBAR COMPONENT LOGIC
-     ========================================================================== */
+   NAVBAR COMPONENT LOGIC
+   ========================================================================== */
 
-import { Component, HostListener } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, HostListener, Renderer2 } from '@angular/core';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+// Importación de componentes compartidos
 import { SocialComponent } from '../../shared/components/social-links/social.component';
 
 @Component({
@@ -15,24 +17,54 @@ import { SocialComponent } from '../../shared/components/social-links/social.com
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  // Estado del menú desplegable en dispositivos móviles
+  // Controlar estado del menú (Abierto / Cerrado)
   public isMenuOpen = false;
 
-  // Alternar estado del menú hamburguesa (Abrir / Cerrar)
+  // Cambiar estilo del menú (Desktop / Mobile)
+  public isScrolled = false;
+
+  // Constructor principal
+  constructor(
+    private renderer: Renderer2,
+    private viewportScroller: ViewportScroller,
+  ) {}
+
+  // Reconocer y modificar estilos del menú
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.pageYOffset || document.documentElement.scrollTop || 0;
+    this.isScrolled = offset > 50;
+  }
+
+  // Alternar estado del menú (Mobile)
   public toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+    // Bloquear o liberar el menú principal
+    if (this.isMenuOpen) {
+      this.renderer.addClass(document.body, 'no-scroll');
+    } else {
+      this.renderer.removeClass(document.body, 'no-scroll');
+    }
   }
 
-  // Cerrar ménu manualmente
+  // Cerrar menú directamente
   public closeMenu(): void {
     this.isMenuOpen = false;
+    this.renderer.removeClass(document.body, 'no-scroll');
   }
 
-  // Cerrar menú automáticamente
+  // Direccionar al usuario a la sección seleccionada
+  public scrollToAnchor(elementId: string): void {
+    this.closeMenu();
+    setTimeout(() => {
+      this.viewportScroller.scrollToAnchor(elementId);
+    }, 150);
+  }
+
+  // Reconocer y modificar estilos del menú
   @HostListener('window:resize', ['$event'])
-  protected onResize(event: UIEvent): void {
-    const window = event.target as Window;
-    if (window.innerWidth > 991 && this.isMenuOpen) {
+  protected onResize(event: any): void {
+    if (event.target.innerWidth > 991 && this.isMenuOpen) {
       this.closeMenu();
     }
   }
