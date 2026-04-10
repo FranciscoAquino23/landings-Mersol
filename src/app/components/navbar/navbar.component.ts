5e4f62d7aa3b623d7889afa7b0788a2a2ef6ec18
Francisco Aquino <francoaquialla@gmail.com>
2026-04-10 13:39:39 -0600

    Navbar component restructured for a multi-landing page creation model

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,29 +2,26 @@
     NAVBAR COMPONENT LOGIC
    ========================================================================== */
 
-import { Component, HostListener, Renderer2 } from '@angular/core';
-import { CommonModule, ViewportScroller } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
-// Importar componentes
+import { Component, HostListener, Input, Renderer2 } from '@angular/core';
 import { SocialComponent } from '../../shared/components/social-links/social.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule, SocialComponent],
+  imports: [SocialComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-// Lógica de navegación (MEDIA QUERIES)
 export class NavbarComponent {
+  // Recibir información del logo para cada landing
+  @Input() brandLogo?: string;
+  @Input() brandLogoAlt = 'Logo de la marca';
+
+  // Controlar visibilidad de la barra de navegación (Desktop/Mobile)
   public isMenuOpen = false;
   public isScrolled = false;
 
-  constructor(
-    private renderer: Renderer2,
-    private viewportScroller: ViewportScroller,
-  ) {}
+  constructor(private renderer: Renderer2) {}
 
   // Detectar movimiento para cambiar el estilo de la barra de navegación
   @HostListener('window:scroll', [])
@@ -52,26 +49,16 @@ export class NavbarComponent {
   // Direccionar correctamente al usuario a la sección seleccionada
   public scrollToAnchor(elementId: string): void {
     this.closeMenu();
-
     setTimeout(() => {
       // Caso 1: Regresar al inicio
       if (elementId === 'home' || elementId === 'inicio') {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
-        });
+        window.scrollTo({ top: 0, behavior: 'smooth' });
       } else {
         // Caso 2: Regresar a alguna sección específica
         const element = document.getElementById(elementId);
         if (element) {
-          const headerOffset = 90;
-          const elementPosition = element.getBoundingClientRect().top;
-          const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-
-          window.scrollTo({
-            top: offsetPosition,
-            behavior: 'smooth',
-          });
+          const offsetPosition = element.getBoundingClientRect().top + window.pageYOffset - 90;
+          window.scrollTo({ top: offsetPosition, behavior: 'smooth' });
         }
       }
     }, 150);
